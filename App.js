9e77cf0136e0069bc41a2a343875b29bb4918861
Ethan van Heerden <ethan@vpoint.biz>
2024-03-14 19:49:56 -0400

    assignment

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
 app.use(cors());
@@ -9,5 +11,7 @@ app.use(express.json());
 
 Hello(app);
 Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
 
 app.listen(4000);
