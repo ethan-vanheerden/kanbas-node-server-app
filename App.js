df1c8597404516b151cd08a9b936eb7dcd5b84d4
Ethan van Heerden <ethan@vpoint.biz>
2024-03-25 19:47:46 -0400

    be-wip

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
 import express from "express";
+import "dotenv/config";
+import session from "express-session";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 
+mongoose.connect("mongodb://localhost:27017/?tls=false&directConnection=true");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
+app.use(session(sessionOptions));
+
 app.use(express.json());
 
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
